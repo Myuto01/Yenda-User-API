bfd4f91aaf27385cc70d605e41875b5a611f050e
Myuto01 <myutotharapper04@gmail.com>
2024-03-28 12:46:00 +0200

    changed the setting.py file back and also removed the new files added incliding Procfile so I can deploy on Render

--- YENDA_API/settings.py
+++ YENDA_API/settings.py
@@ -4,6 +4,7 @@ import os
 load_dotenv()
 import django_heroku
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -15,7 +16,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
