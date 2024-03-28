83e814764999e8011716f95ec671ca1aa9445c6b
Myuto01 <myutotharapper04@gmail.com>
2024-03-28 08:50:45 +0200

    changed requirements.txt

--- YENDA_API/settings.py
+++ YENDA_API/settings.py
@@ -17,7 +17,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["*, yenda-user-api-bad6be693b0c.herokuapp.com"]
 
 AUTH_USER_MODEL = "main.User"
 
